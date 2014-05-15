7297cd816888349ff57f396a228f95cb77cdf402
danielnowendsztern <danielnowens@hotmail.com>
2014-05-15 19:27:31 +0200

    Resto de instrucciones (Comprobar bool's de >=,>,< y <= )

---
+++ src/maquinap/instrucciones/comparacion/Igual.java
@@ -0,0 +1,27 @@
+package maquinap.instrucciones.comparacion;
+
+import java.util.Stack;
+
+import maquinap.MaquinaP;
+import maquinap.instrucciones.Instruccion;
+import maquinap.valor.Bool;
+import maquinap.valor.Valor;
+
+public class Igual extends Instruccion {
+	
+	@Override
+	public void ejecutar(MaquinaP maq) {
+		Stack<Valor<?>> pe = maq.getPilaEvaluacion();
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor1 = pe.pop();
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor2 = pe.pop();
+		if (!(valor2.getValor().getClass().equals(valor1.getValor().getClass()))){
+			return;
+		}
+		Bool newValue = new Bool(valor2.getValor()==valor1.getValor());
+		maq.getPilaEvaluacion().push(newValue);
+		maq.aumentarContadorPrograma(1);
+	}
+
+}
---
+++ src/maquinap/instrucciones/comparacion/Mayor.java
@@ -0,0 +1,35 @@
+package maquinap.instrucciones.comparacion;
+
+import java.util.Stack;
+
+import maquinap.MaquinaP;
+import maquinap.instrucciones.Instruccion;
+import maquinap.valor.Bool;
+import maquinap.valor.Valor;
+
+public class Mayor extends Instruccion {
+	
+	@Override
+	public void ejecutar(MaquinaP maq) {
+		Stack<Valor<?>> pe = maq.getPilaEvaluacion();
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor1 = pe.pop();
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor2 = pe.pop();
+		if (!(valor2.getValor().getClass().equals(valor1.getValor().getClass()))){
+			return;
+		}
+		boolean comparacion;
+		if (valor1.getValor().getClass().equals(Boolean.class)){
+			// true > false
+			comparacion = (! (!(Boolean) valor1.getValor() && ((Boolean) valor2.getValor())));
+		} else {
+			comparacion = (Integer) valor1.getValor() < (Integer) valor2.getValor();
+		}
+		
+		Bool newValue = new Bool(comparacion);
+		maq.getPilaEvaluacion().push(newValue);
+		maq.aumentarContadorPrograma(1);
+	}
+
+}
