4b11107cfbcf71428bef5a3bf7edfe685f066bcc
danielnowendsztern <danielnowens@hotmail.com>
2014-05-15 19:04:47 +0200

    Agregadas instrucciones lógicas.

---
+++ src/maquinap/instrucciones/logicas/Or.java
@@ -0,0 +1,30 @@
+package maquinap.instrucciones.logicas;
+
+import java.util.Stack;
+
+import maquinap.MaquinaP;
+import maquinap.instrucciones.Instruccion;
+import maquinap.valor.Bool;
+import maquinap.valor.Valor;
+
+public class Or extends Instruccion {
+	
+	@Override
+	public void ejecutar(MaquinaP maq) {
+		Stack<Valor<?>> pe = maq.getPilaEvaluacion();
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor1 = pe.pop();
+		if (!(valor1.getValor() instanceof Boolean)){
+			return;
+		}
+		if (pe.isEmpty()){ return; }		
+		Valor<?> valor2 = pe.pop();
+		if (!(valor2.getValor() instanceof Boolean)){
+			return;
+		}
+		Bool newValue = new Bool((Boolean)valor2.getValor()||(Boolean)valor1.getValor());
+		maq.getPilaEvaluacion().push(newValue);
+		maq.aumentarContadorPrograma(1);
+	}
+
+}
