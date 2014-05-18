1a1444af31994397e7e316cf9a8880f4fedde58a
Vik <Vik@192.168.1.38>
2014-05-18 22:09:42 +0200

    Libera mejorado para que libere memoria

--- src/maquinap/instrucciones/memoria/Libera.java
+++ src/maquinap/instrucciones/memoria/Libera.java
@@ -5,123 +5,154 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import maquinap.MaquinaP;
 import maquinap.instrucciones.Instruccion;
+import maquinap.valor.Int;
+import maquinap.valor.Valor;
 
 public class Libera extends Instruccion {
 
 	private Integer cantidad;
-	
+
 	public Libera(Integer cantidad) {
 		this.cantidad = cantidad;
-	}	
-	
+	}
+
 	@Override
 	public void ejecutar(MaquinaP maq) throws Exception {
 		int dirPila = (Integer) maq.getPilaEvaluacion().pop().getValor();
 		List<Espacio> esp = maq.getListaDeEspacios();
-		
+
 		// Uso copias para no liarla.
 		int dirPilaTmp = dirPila;
 		int cantidadTmp = this.cantidad;
+		Map<Integer, Valor<?>> me = maq.getMemoriaDatos();
+		int ini = -1, fin = -1;
+		Collections.sort(maq.getListaDeEspacios(), comparator);
 
-		Collections.sort(maq.getListaDeEspacios(), comparator);	
-		
-		for (Espacio e : esp){
-			if (e.estaEnEsteEspacioDeDirecciones(dirPilaTmp)){
-				if (e.superaElTamanio(dirPilaTmp, cantidadTmp)){		
-					if (e.getDir_com() < dirPilaTmp){
+		for (Espacio espacio : esp) {
+			if (espacio.estaEnEsteEspacioDeDirecciones(dirPilaTmp)) {
+				if (espacio.superaElTamanio(dirPilaTmp, cantidadTmp)) {
+					if (espacio.getDir_com() < dirPilaTmp) {
 						// Hay que dividir en dos partes
+
+						int tam = dirPilaTmp - espacio.getDir_com();
+						esp.add(new Espacio(espacio.getDir_com(), tam, espacio
+								.isLibre()));
+
+						int tam2 = (espacio.getDir_com() + espacio.getTam())
+								- dirPilaTmp;
+						espacio.setTam(tam2);
+						espacio.setDir_com(dirPilaTmp);
+						espacio.setLibre(true);
 						
-						int tam = dirPilaTmp - e.getDir_com();
-						esp.add(new Espacio(e.getDir_com(), tam, e.isLibre()));
-						
-						int tam2 = (e.getDir_com()+e.getTam())-dirPilaTmp;
-						e.setTam(tam2);
-						e.setDir_com(dirPilaTmp);
-						e.setLibre(true);
+						ini = espacio.getDir_com();
+						fin = ini + espacio.getTam();
 
 						dirPilaTmp += tam2;
 						cantidadTmp -= tam2;
-						
-					} else /* Tienen la misma direccion */ {
-						e.setLibre(true);
-						dirPilaTmp += e.getTam();
-						cantidadTmp -= e.getTam();
-					}	
-					
-				} else if (e.loLiberaExacto(dirPilaTmp, cantidadTmp)){
-					if (e.getDir_com() < dirPilaTmp){
-						// Hay que dividir en dos partes
-						
-						int tam = dirPilaTmp - e.getDir_com();
-						esp.add(new Espacio(e.getDir_com(), tam, e.isLibre()));
-						
-						int tam2 = (e.getDir_com()+e.getTam())-dirPilaTmp;
-						e.setTam(tam2);
-						e.setDir_com(dirPilaTmp);
-						e.setLibre(true);
 
+					} else /* Tienen la misma direccion */{
+						espacio.setLibre(true);
+						dirPilaTmp += espacio.getTam();
+						cantidadTmp -= espacio.getTam();
+
+						ini = espacio.getDir_com();
+						fin = ini + espacio.getTam();
+					}
+
+				} else if (espacio.loLiberaExacto(dirPilaTmp, cantidadTmp)) {
+					if (espacio.getDir_com() < dirPilaTmp) {
+						// Hay que dividir en dos partes
+
+						int tam = dirPilaTmp - espacio.getDir_com();
+						esp.add(new Espacio(espacio.getDir_com(), tam, espacio
+								.isLibre()));
+
+						int tam2 = (espacio.getDir_com() + espacio.getTam())
+								- dirPilaTmp;
+						espacio.setTam(tam2);
+						espacio.setDir_com(dirPilaTmp);
+						espacio.setLibre(true);
+
+						ini = espacio.getDir_com();
+						fin = ini + espacio.getTam();
+						
 						dirPilaTmp += tam2;
 						cantidadTmp -= tam2;
+
+					} else /* Tienen la misma direccion <-> caso ideal */{
+						espacio.setLibre(true);
+						dirPilaTmp += espacio.getTam();
+						cantidadTmp -= espacio.getTam();
 						
-					} else /* Tienen la misma direccion <-> caso ideal */ {
-						e.setLibre(true);
-						dirPilaTmp += e.getTam();
-						cantidadTmp -= e.getTam();
-					}			
+
+						ini = espacio.getDir_com();
+						fin = ini + espacio.getTam();
+					}
 					break;
-					
-				} else /* No lo libera completamente */ {		
+
+				} else /* No lo libera completamente */{
 					// Hay que dividir en tres partes
+
+					int tam1 = dirPilaTmp - espacio.getDir_com();
+					esp.add(new Espacio(espacio.getDir_com(), tam1, espacio
+							.isLibre()));
+
+					int tam3 = espacio.getDir_com() - dirPilaTmp + cantidadTmp;
+					esp.add(new Espacio(dirPilaTmp + cantidadTmp, tam3, espacio
+							.isLibre()));
+
+					espacio.setLibre(true);
+					espacio.setTam(espacio.getTam() - tam1 - tam3);
+					espacio.setDir_com(espacio.getDir_com() + tam1);
+
+
+					ini = espacio.getDir_com();
+					fin = ini + espacio.getTam();
 					
-					int tam1 = dirPilaTmp - e.getDir_com();
-					esp.add(new Espacio(e.getDir_com(), tam1, e.isLibre()));
-					
-					int tam3 = e.getDir_com() - dirPilaTmp + cantidadTmp;
-					esp.add(new Espacio(dirPilaTmp+cantidadTmp, tam3, e.isLibre()));
-					
-					e.setLibre(true);
-					e.setTam(e.getTam()-tam1-tam3);
-					e.setDir_com(e.getDir_com()+tam1);
-					
-					dirPilaTmp += e.getTam();
-					cantidadTmp -= e.getTam();
-					
+					dirPilaTmp += espacio.getTam();
+					cantidadTmp -= espacio.getTam();
+
 					break;
 				}
 			}
 		}
 		
-		/* fusiona y simplifica la lista de espacios. */		
+		while (ini < fin){
+			maq.getMemoriaDatos().put(ini++, new Int(0));
+		}
+
+		/* fusiona y simplifica la lista de espacios. */
 		ArrayList<Espacio> espacios = new ArrayList<Espacio>();
-		
-		Collections.sort(esp, comparator);		
+
+		Collections.sort(esp, comparator);
 		Iterator<Espacio> it = esp.iterator();
 		Espacio eAnt = it.next();
-		while (it.hasNext()){
-			Espacio e = it.next();
-			if (eAnt.isLibre() && e.isLibre()){
-				e.setDir_com(eAnt.getDir_com());
-				e.setTam(e.getTam()+eAnt.getTam());
-				espacios.add(eAnt);				
+		while (it.hasNext()) {
+			Espacio espacio = it.next();
+			if (eAnt.isLibre() && espacio.isLibre()) {
+				espacio.setDir_com(eAnt.getDir_com());
+				espacio.setTam(espacio.getTam() + eAnt.getTam());
+				espacios.add(eAnt);
 			}
-			eAnt = e;
+			eAnt = espacio;
 		}
-		
-		for (Espacio e : espacios){
-			esp.remove(e);
+
+		for (Espacio espacio : espacios) {
+			esp.remove(espacio);
 		}
-		
-		maq.aumentarContadorPrograma(1);		
-	}	
-	
+
+		maq.aumentarContadorPrograma(1);
+	}
+
 	private static final Comparator<Espacio> comparator = new Comparator<Espacio>() {
 		@Override
 		public int compare(Espacio e1, Espacio e2) {
 			return e1.compareTo(e2);
-		}		
-	}; 	
+		}
+	};
 
 }
